cedc0fa766da01a9e0be6da9e0f8e3104bf95d13
George Weiner <gecovin@protonmail.com>
2022-01-17 16:58:14 +0100

    Made various improvements to the config.

--- autostart.sh
+++ autostart.sh
@@ -37,18 +37,18 @@ dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
 xsetroot -cursor_name left_ptr &
 
 conky -c $HOME/.config/bspwm/system-overview &
-run variety &
+#run variety &
 run nm-applet &
 run pamac-tray &
 run xfce4-power-manager &
 numlockx on &
 blueberry-tray &
-sh $HOME/.config/polybar/launch.sh --grayblocks &
+sh $HOME/.config/polybar/launch.sh --hack &
 picom --config $HOME/.config/bspwm/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 run volumeicon &
-#nitrogen --restore
+nitrogen --restore
 #run caffeine &
 #run vivaldi-stable &
 #run firefox &
